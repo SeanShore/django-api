3b9039fb67840de7d8a20fe0523df0ba27729206
SeanShore <sean@shoremonitoring.nl>
2020-04-18 22:02:25 +0100

    Configured postgres

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,7 +22,8 @@ class UserAdmin(BaseUserAdmin):
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
-            }),
+        }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
