1bede42a5e5b64fcec9859636e064283e2bce6be
SeanShore <sean@shoremonitoring.nl>
2020-04-20 09:29:29 +0100

    Added tag create

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -9,7 +7,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """ Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -19,3 +19,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """ Return objects for current auth user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Crate a new tag """
+        serializer.save(user=self.request.user)
