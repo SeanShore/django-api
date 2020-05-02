e08dae0fca9f57d803299a4bfc74313bdd3cab1c
SeanShore <sean@shoremonitoring.nl>
2020-05-02 12:30:38 +0100

    Recipe object added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -60,3 +60,13 @@ class ModelTest(TestCase):
             name="Cocumber"
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_to_str(self):
+        """ Test the recipe string representatie"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=10.00
+        )
+        self.assertEqual(str(recipe),recipe.title)
